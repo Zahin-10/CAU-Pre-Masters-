93a61dad6f8e5e7601708e861fd02a0fbe7b11f1
Tahmid Zahin <tzahin10@gmail.com>
2019-10-30 14:00:53 +0100

    Add Remove to BST

--- SearchTree/src/Main.java
+++ SearchTree/src/Main.java
@@ -8,7 +8,9 @@ public class Main {
 		bstOne.addNode(50);
 		bstOne.addNode(45);
 		bstOne.addNode(47);
+		bstOne.addNode(46);
 		bstOne.addNode(42);
+        bstOne.removeNode(45);
 		int[] myTreeArray = new int[bstOne.size];
 		bstOne.inorderToArray(bstOne.root,myTreeArray);
 		System.out.println(Arrays.toString(myTreeArray));
--- SearchTree/src/Tree.java
+++ SearchTree/src/Tree.java
@@ -57,5 +57,35 @@ public class Tree {
 		}
 		return found;
 	}
+
+	public void removeNode(int value){
+		Node node = getNodeToBeRemoved(value);
+		Node min = getMinOfRightTree(node);
+		System.out.println(min.value);
+		node.value = min.left.value;
+		min.left = null;
+		this.size--;
+	}
+
+	Node getNodeToBeRemoved(int value){
+		while(this.traverse.value != value){
+			if(value < this.traverse.value){
+				this.traverse = this.traverse.left;
+			}else{
+				this.traverse = this.traverse.right;
+			}
+		}
+		Node search = this.traverse;
+		this.traverse = this.root;
+		return search;
+	}
+
+	Node getMinOfRightTree(Node node){
+		while (node.left.left != null)
+		{
+			node = node.left;
+		}
+		return node;
+	}
 	
 }
