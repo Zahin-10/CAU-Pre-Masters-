e82e37baafb0be55c318c6cfd8b9c289b7c0d63d
Tahmid Zahin <tzahin10@gmail.com>
2019-10-30 14:37:53 +0100

    get nth prime number

--- NthPrime/src/com/uri/Main.java
+++ NthPrime/src/com/uri/Main.java
@@ -4,16 +4,30 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int n = 5;
+        int n = 11;
         int count = 0;
-        int i = 0;
-        int sqrt = Math.sqrt()
-        while (count < n){
-
+        int i =2;
+        while(true)
+        {
+            if(checkPrime(i)){
+                count++;
+            }
+            if(count == n)
+            {
+                System.out.println("This is the nth prime number: "+i);
+                break;
+            }
+            i++;
         }
     }
 
     public static boolean checkPrime(int number){
-
+        int sqrt = (int) Math.floor(Math.sqrt(number));
+        for(int i=2;i<= sqrt;i++){
+            if(number % i == 0){
+                return false;
+            }
+        }
+        return true;
     }
 }
